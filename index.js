43c4f079fcff3f47d3979397395c8db812465d22
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-02-01 11:03:54 +0500

    Simple Calculator

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 alert("Assalomu alaykum");
 alert("Dasturimizga xush kelibsiz!");
 alert("Bu yerda siz tekin kalkulyator ishlatishingiz mumkin");
+
 const num1 = document.querySelector(".inputNum1");
 const num2 = document.querySelector(".inputNum2");
 const answer = document.querySelector(".answer");
-
 const plus = document.querySelector(".button_1");
 const minus = document.querySelector(".button_2");
 const mult = document.querySelector(".button_3");
